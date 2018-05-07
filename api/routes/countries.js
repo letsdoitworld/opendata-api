7e9e28b2a551fc3ecf12e536276ae1699ec4abf3
Aleksandr Ivanov <aleksandr@wemakesoftware.eu>
2018-05-07 11:21:14 +0300

    added countries API endpoint with countries statistics (number of reports, country code, tpr index)

--- api/routes/countries.js
+++ api/routes/countries.js
@@ -5,14 +5,20 @@ const db = require('../db');
 const router = new Router();
 
 router.get('/', async (req, res) => {
-  const query = 'select count (*) as reports_number, country_code from reports where country_code is not null group by country_code order by 1 desc;';
+  const query = 'select count (*) as reports_number' +
+      ', r.country_code' +
+      ', count(*) /  (select population / 10000 from country_population where country_population.country_code = r.country_code) as tpr' +
+      ' from reports r' +
+      ' where  r.country_code is not null' +
+      ' group by r.country_code order by tpr desc NULLS  LAST;';
   const { rows } = await db.query(query);
   res.send({
     'status': 'SUCCESS',
       'sources': rows.map(v => {
           return { 
             "count": v["reports_number"],
-            "country_code": v["country_code"]
+            "country_code": v["country_code"],
+            "tpr": v["tpr"]
           };
       })
   })
