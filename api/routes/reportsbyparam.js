8915c8d16d03bd758549b1564059e02509c04425
eugen.knyazev <eugen.knyazev@wemakesoftware.eu>
2018-05-11 21:25:37 +0300

    total count added to reports query

--- api/routes/reportsbyparam.js
+++ api/routes/reportsbyparam.js
@@ -9,37 +9,37 @@ router.get('/', async (req, res) => {
     const { rows } = await query(req.query.code?req.query.code.toUpperCase():req.query.code, req.query.createdby,
         req.query.start, req.query.end, req.query.maxrec, req.query.startrecord);
     res.send({
-        'status': 'SUCCESS',
-        'sources': rows
+        'trashpoints_total': rows[0].full_count,
+        'trashpoints': rows
     })
 });
 var query = (country_code, created_by, start_date , end_date, max_records, start_from_record) => {
-    let q = 'SELECT * FROM reports';
+    let q = 'select *, count(*) over() as full_count from reports';
     let parExist=false;
     let elem=0
     let params = [country_code, created_by, start_date?convertDate(start_date):null,
         end_date?convertDate(end_date):null, start_from_record, max_records];
     if (country_code) {
         elem=elem+1;
-        q = q + ' WHERE country_code= $'+elem;
+        q = q + ' where country_code= $'+elem;
         parExist=true;
     } else {params.splice(params.indexOf(country_code),1)}
     if (created_by) {
         elem=elem+1;
-        q = q + (parExist?' AND':' WHERE')+' created_by=$'+elem;
+        q = q + (parExist?' and':' where')+' created_by=$'+elem;
         parExist=true;
     } else {params.splice(params.indexOf(created_by),1)}
     if (start_date) {
         elem=elem+1;
-        q = q + (parExist?' AND':' WHERE')+' last_updated>=$'+elem;
+        q = q + (parExist?' and':' where')+' last_updated>=$'+elem;
         parExist=true;
     } else {params.splice(params.indexOf(start_date),1)}
     if (end_date) {
         elem=elem+1;
-        q = q + (parExist?' AND':' WHERE')+' last_updated<=$'+elem;
+        q = q + (parExist?' and':' where')+' last_updated<=$'+elem;
         parExist=true;
     } else {params.splice(params.indexOf(end_date),1)}
-    q = q + ' ORDER BY last_updated';
+    q = q + ' order by last_updated';
     if (start_from_record) {
         elem=elem+1;
         q = q + ' offset $'+elem;
