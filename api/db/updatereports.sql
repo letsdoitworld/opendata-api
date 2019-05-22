95f16214e563aa5e10da61979756d5b658060007
Kris Haamer <kris@haam.co>
2019-05-22 13:13:41 +0300

    Remove drop

--- api/db/updatereports.sql
+++ api/db/updatereports.sql
@@ -1,5 +1,6 @@
 ALTER TABLE reports RENAME COLUMN ts TO ts_imported;
 ALTER TABLE reports ADD COLUMN last_updated TIMESTAMP WITH TIME ZONE;
 ALTER TABLE reports ADD FOREIGN KEY (type) REFERENCES resource(name);
-ALTER TABLE country_population  DROP COLUMN reportsqnt;
 ALTER TABLE country_population  ADD COLUMN reports_qnt int;
+
+
